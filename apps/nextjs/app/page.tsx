d96393dc152743fada04cc378f4d2585e1584d3b
hjcasayas <henlyjade.casayas@gmail.com>
2024-04-07 16:59:09 +0000

    fix: package name and import

--- apps/nextjs/app/page.tsx
+++ apps/nextjs/app/page.tsx
@@ -1,6 +1,6 @@
-import { greet } from "domain";
+import { greet } from "@hjcasayas/domain";
 
 export default function AppPage() {
-  const greeting = greet("Aziel");
+  const greeting = greet("Jose!");
   return <p>{greeting}</p>;
 }
